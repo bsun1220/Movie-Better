92da5d95ef65b0d49539686508b794c54e02dc87
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2022-10-18 17:35:13 -0400

    began datavis page

--- client/src/components/datavisual/datavispage.js
+++ client/src/components/datavisual/datavispage.js
@@ -11,8 +11,28 @@ export default function DataVisPage(){
             <p style = {{"marginTop":"10px"}}>Quant Research</p>
         </div>
         <div className = {"aboutpage"}>
-            <h1>How to Use</h1>
+            <div style = {{"display":"flex", "alignItems":"center"}}>
+                <img src = "questionmark.svg" height = "40px"/>
+                <h1 style = {{"marginLeft": "20px"}}>How To Use</h1>
+            </div>
             <hr/>
+            <p> The main purpose of DataVis is to allow potential bettors to 
+                research quantitative trends behind movies they are hoping to predict values for. Through careful analysis, from
+                examining past genre time series analysis, director rating, and actor rating, DataVis ensures that users are able
+                to fully understand all the information they need to know to decide what their predicted value should be.
+            </p>
+            <p>
+                The steps for this tool is as follows. First, the user will select a movie in 2023 that they want to conduct analysis on. 
+                From there, the user will receive basic information from this movie, including genre, title, director, and actors. From there,
+                The user can continue to conduct future analysis. For example, the user can examine the past movies from the director in the form 
+                of time series analysis. Other features include search by actor and genre analysis. From there, the user can add more restrictions
+                within their search, allowing for more pinpoint precision. This data originates from IMDB.
+            </p>
+            <p>
+                After conducting this analysis. A user can examine other trends such as the total number of bets being placed on the 2023 movie
+                to find if they believe that the general consensus on this movie is incorrect or not. After they have decided fully 
+                on what the predicted value is, the user can place a bet on the separate betting platform page.
+            </p>
         </div>
 
     </div>)
