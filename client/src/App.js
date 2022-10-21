05e8c98179e05c42fb07aad769684370509ff73e
reaganrazon <reaganrazon@gmail.com>
2022-10-21 14:31:49 -0400

    deleted test endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,6 @@ import RatingEndpoint from "./ratingendpoint";
 import HomePage from "./components/homepage/home";
 import DataVisPage from "./components/datavisual/datavispage";
 import TopMovies from "./topmovies";
-import Actor from "./components/recentmovies/actor.js";
-import Director from "./components/recentmovies/director.js";
 const App = () => {
     return (
         <div>
@@ -21,8 +19,6 @@ const App = () => {
                 <Route exact path = "/" element = {<HomePage/>}/>
                 <Route exact path = "/datavis" element = {<DataVisPage/>}/>
                 <Route exact path = "/topmovies" element = {<TopMovies/>}/>
-                <Route exact path = "/recentmovies/actor" element = {<Actor/>}/>
-                <Route exact path = "/recentmovies/director" element = {<Director/>}/>
             </Routes>
         </div>
     );
